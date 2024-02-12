7aaaf59267e6b2bb42692e7f4bdee6c5e89ed5ac
Unique Neupane <133880428+Unique982@users.noreply.github.com>
2024-02-12 18:12:08 +0545

    Add files via upload

---
+++ JavaScript Lab 1/example 1b.js
@@ -0,0 +1,15 @@
+document.write("files are linked");
+document.write("<br/>");
+document.write("for Var");
+document.write("<br/>");
+document.write(x);
+var x=10;
+
+document.write(x);
+document.write("<br/>");
+document.write("for let");
+document.write("<br/>");
+
+let y=10;
+document.write("    ");
+document.write(y);
